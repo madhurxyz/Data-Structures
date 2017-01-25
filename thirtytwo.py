a91324e6fe17908d9aa9eb329cc45d026cf023d1
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-25 09:38:28 -0800

    iterating on thirtytwo.py and bass.py, 2 out of 3 tests passed

--- thirtytwo.py
+++ thirtytwo.py
@@ -1,133 +1,93 @@
 def encode_thirtytwo(remainder):
-    if remainder is 0:
-        remainder = "a"
-    if remainder is 1:
-        remainder = "b"
-    if remainder is 2:
-        remainder = "c"
-    if remainder is 3:
-        remainder = "d"
-    if remainder is 4:
-        remainder = "e"
-    if remainder is 5:
-        remainder = "f"
-    if remainder is 6:
-        remainder = "g"
-    if remainder is 7:
-        remainder = "h"
-    if remainder is 8:
-        remainder = "i"
-    if remainder is 9:
-        remainder = "j"
     if remainder is 10:
-        remainder = "k"
+        remainder = "a"
     if remainder is 11:
-        remainder = "l"
+        remainder = "b"
     if remainder is 12:
-        remainder = "m"
+        remainder = "c"
     if remainder is 13:
-        remainder = "n"
+        remainder = "d"
     if remainder is 14:
-        remainder = "o"
+        remainder = "e"
     if remainder is 15:
-        remainder = "p"
+        remainder = "f"
     if remainder is 16:
-        remainder = "q"
+        remainder = "g"
     if remainder is 17:
-        remainder = "r"
+        remainder = "h"
     if remainder is 18:
-        remainder = "s"
+        remainder = "i"
     if remainder is 19:
-        remainder = "t"
+        remainder = "k"
     if remainder is 20:
-        remainder = "u"
+        remainder = "m"
     if remainder is 21:
-        remainder = "v"
+        remainder = "n"
     if remainder is 22:
-        remainder = "w"
+        remainder = "p"
     if remainder is 23:
-        remainder = "x"
+        remainder = "q"
     if remainder is 24:
-        remainder = "y"
+        remainder = "r"
     if remainder is 25:
-        remainder = "z"
+        remainder = "s"
     if remainder is 26:
-        remainder = "2"
+        remainder = "t"
     if remainder is 27:
-        remainder = "3"
+        remainder = "v"
     if remainder is 28:
-        remainder = "4"
+        remainder = "w"
     if remainder is 29:
-        remainder = "5"
+        remainder = "x"
     if remainder is 30:
-        remainder = "6"
+        remainder = "y"
     if remainder is 31:
-        remainder = "7"
+        remainder = "z"
     return remainder
 
 def decode_thirtytwo(num):
     if num is "a":
-        num = 0
-    if num is "b":
-        num = 1
-    if num is "c":
-        num = 2
-    if num is "d":
-        num = 3
-    if num is "e":
-        num = 4
-    if num is "f":
-        num = 5
-    if num is "g":
-        num = 6
-    if num is "h":
-        num = 7
-    if num is "i":
-        num = 8
-    if num is "j":
-        num = 9
-    if num is "k":
         num = 10
-    if num is "l":
+    if num is "b":
         num = 11
-    if num is "m":
+    if num is "c":
         num = 12
-    if num is "n":
+    if num is "d":
         num = 13
-    if num is "o":
+    if num is "e":
         num = 14
-    if num is "p":
+    if num is "f":
         num = 15
-    if num is "q":
+    if num is "g":
         num = 16
-    if num is "r":
+    if num is "h":
         num = 17
-    if num is "s":
+    if num is "i":
         num = 18
-    if num is "t":
+    if num is "k":
         num = 19
-    if num is "u":
+    if num is "m":
         num = 20
-    if num is "v":
+    if num is "n":
         num = 21
-    if num is "w":
+    if num is "p":
         num = 22
-    if num is "x":
+    if num is "q":
         num = 23
-    if num is "y":
+    if num is "r":
         num = 24
-    if num is "z":
+    if num is "s":
         num = 25
-    if num is "2":
+    if num is "t":
         num = 26
-    if num is "3":
+    if num is "v":
         num = 27
-    if num is "4":
+    if num is "w":
         num = 28
-    if num is "5":
+    if num is "x":
         num = 29
-    if num is "6":
+    if num is "y":
         num = 30
-    if num is "7":
+    if num is "z":
         num = 31
     return num
