1c1df9d25b3c4e4cdb6f32c352c56eac340ddd73
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-27 12:07:30 -0800

    successfully implemented iterative binary search

--- search.py
+++ search.py
@@ -4,8 +4,8 @@ def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
-    # return linear_search_recursive(array, item)
+    # return linear_search_iterative(array, item)
+    return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -40,11 +40,26 @@ def binary_search(array, item):
 
 
 def binary_search_iterative(array, item):
-    # TODO: implement binary search iteratively here
-    pass
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests below
+    left = 0
+    right = len(array) - 1
+    found = False
+    half = 0
 
+    while left<=right and not found:
+        half = (left+right)/2
+        if array[half] == item:
+            found = True
+        elif item < array[half]:
+            right = half - 1
+        elif item > array[half]:
+            left = half + 1
+
+    if found:
+        return half
+    elif not found:
+        return None    
 
 def binary_search_recursive(array, item, left=None, right=None):
     # TODO: implement binary search recursively here
