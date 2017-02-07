0d99cd0ee675b69c371ea02d2c72647ae45935be
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-07 09:27:36 -0800

    updated stack and queue implementations

--- queue.py
+++ queue.py
@@ -1,11 +1,63 @@
 #!python
+from linkedlist import LinkedList
+
+# enqueue - append to tail
+# dequeue - remove from head
+# peek - examine first
+
+
+# class Queue(object):
+#
+#     def __init__(self, iterable=None):
+#         """Initialize this queue and enqueue the given items, if any"""
+#         self.queue = []
+#         if iterable:
+#             for item in iterable:
+#                 self.enqueue(item)
+#
+#     def __repr__(self):
+#         """Return a string representation of this queue"""
+#         return 'Queue({})'.format(self.length())
+#
+#     #O(1)
+#     def is_empty(self):
+#         """Return True if this queue is empty, or False otherwise"""
+#         if len(self.queue) == 0:
+#             return True
+#         return False
+#
+#     #O(1)
+#     def length(self):
+#         """Return the number of items in this queue"""
+#         return len(self.queue)
+#
+#     #O(1)
+#     def peek(self):
+#         """Return the next item in this queue without removing it,
+#         or None if this queue is empty"""
+#         if self.is_empty():
+#             return None
+#         return self.queue[0]
+#
+#     #O(1)
+#     def enqueue(self, item):
+#         """Enqueue the given item into this queue"""
+#         self.queue.append(item)
+#
+#     #O(1)
+#     def dequeue(self):
+#         """Return the next item and remove it from this queue,
+#         or raise ValueError if this queue is empty"""
+#         if self.is_empty():
+#             raise ValueError
+#         else:
+#             return self.queue.pop(0)
 
 class Queue(object):
 
     def __init__(self, iterable=None):
         """Initialize this queue and enqueue the given items, if any"""
-        # TODO: initialize instance variables
-        pass
+        self.queue = LinkedList()
         if iterable:
             for item in iterable:
                 self.enqueue(item)
@@ -14,29 +66,38 @@ class Queue(object):
         """Return a string representation of this queue"""
         return 'Queue({})'.format(self.length())
 
+    #O(1)
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise"""
-        # TODO: check if empty
-        pass
+        if self.queue.length() == 0:
+            return True
+        return False
 
+    #O(1)
     def length(self):
         """Return the number of items in this queue"""
-        # TODO: count number of items
-        pass
+        return self.queue.length()
 
+    #O(1)
     def peek(self):
         """Return the next item in this queue without removing it,
         or None if this queue is empty"""
-        # TODO: return next item, if any
-        pass
+        if self.is_empty():
+            return None
+        return self.queue.head.data
 
+    #O(1)
     def enqueue(self, item):
         """Enqueue the given item into this queue"""
-        # TODO: enqueue given item
-        pass
+        self.queue.append(item)
 
+    #O(n)
     def dequeue(self):
         """Return the next item and remove it from this queue,
         or raise ValueError if this queue is empty"""
-        # TODO: dequeue next item, if any
-        pass
+        if self.is_empty():
+            raise ValueError
+        else:
+            first_item = self.queue.head.data
+            self.queue.delete(first_item)
+            return first_item
--- stack.py
+++ stack.py
@@ -1,10 +1,62 @@
 #!python
+from linkedlist import LinkedList
+# push to end
+# pop from end
+# peek end
+
+# class Stack(object):
+#
+#     def __init__(self, iterable=None):
+#         """Initialize this stack and push the given items, if any"""
+#         self.stack = []
+#         pass
+#         if iterable:
+#             for item in iterable:
+#                 self.push(item)
+#
+#     def __repr__(self):
+#         """Return a string representation of this stack"""
+#         return 'Stack({})'.format(self.length())
+#
+#     #O(1)
+#     def is_empty(self):
+#         """Return True if this stack is empty, or False otherwise"""
+#         if len(self.stack) == 0:
+#             return True
+#         return False
+#
+#     #O(1)
+#     def length(self):
+#         """Return the number of items in this stack"""
+#         return len(self.stack)
+#
+#     #O(1)
+#     def peek(self):
+#         """Return the top item on this stack without removing it,
+#         or None if this stack is empty"""
+#         if self.is_empty():
+#             return None
+#         return self.stack[self.length() - 1]
+#
+#     #O(1)
+#     def push(self, item):
+#         """Push the given item onto this stack"""
+#         self.stack.append(item)
+#
+#     #O(1)
+#     def pop(self):
+#         """Return the top item and remove it from this stack,
+#         or raise ValueError if this stack is empty"""
+#         if self.is_empty():
+#             raise ValueError
+#         else:
+#             return self.stack.pop()
 
 class Stack(object):
 
     def __init__(self, iterable=None):
         """Initialize this stack and push the given items, if any"""
-        # TODO: initialize instance variables
+        self.stack = LinkedList()
         pass
         if iterable:
             for item in iterable:
@@ -14,29 +66,38 @@ class Stack(object):
         """Return a string representation of this stack"""
         return 'Stack({})'.format(self.length())
 
+    #O(1)
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise"""
-        # TODO: check if empty
-        pass
+        if self.stack.length() == 0:
+            return True
+        return False
 
+    #O(1)
     def length(self):
         """Return the number of items in this stack"""
-        # TODO: count number of items
-        pass
+        return self.stack.length()
 
+    #O(1)
     def peek(self):
         """Return the top item on this stack without removing it,
         or None if this stack is empty"""
-        # TODO: return top item, if any
-        pass
+        if self.is_empty():
+            return None
+        return self.stack.tail.data
 
+    #O(1)
     def push(self, item):
         """Push the given item onto this stack"""
-        # TODO: push given item
-        pass
+        self.stack.append(item)
 
+    #O(n)
     def pop(self):
         """Return the top item and remove it from this stack,
         or raise ValueError if this stack is empty"""
-        # TODO: pop top item, if any
-        pass
+        if self.is_empty():
+            raise ValueError
+        else:
+            first_item = self.stack.tail.data
+            self.stack.delete(first_item)
+            return first_item
